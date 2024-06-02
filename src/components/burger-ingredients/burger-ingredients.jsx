9786feef2ffc1f5df50b96ecbd9817c454b895ac
asharabanova <n.sharabanova@gmail.com>
2024-06-02 23:55:11 +0300

    active tab feature

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,5 +1,5 @@
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import styles from './burger-ingredients.module.css'
 import IngredientCard from './ingredient-card/ingredient-card'
@@ -28,14 +28,17 @@ export default function BurgerIngredients() {
     const ingredients = useSelector(getIngredients)
     const dispatch = useDispatch()
 
-    useEffect(() => {
-        dispatch(loadIngredients())
-    }, [])
-
     const [activeTabType, setActiveTabType] = useState(ingredientTypes[0].type)
     const [activeIngredient, setActiveIngredient] = useState(null)
 
     const { isModalOpen, openModal, closeModal } = useModal()
+
+    const tabsRef = useRef(null)
+    const titlesRefs = useRef([])
+
+    useEffect(() => {
+        dispatch(loadIngredients())
+    }, [])
 
     const ingredientsDict = ingredients.reduce((dict, ingredient) => {
         if (!dict[ingredient.type]) {
@@ -56,33 +59,57 @@ export default function BurgerIngredients() {
         closeModal()
     }
 
+    const scrollHandler = () => {
+        const tabsBottom = tabsRef.current.getBoundingClientRect().bottom
+
+        const diffs = titlesRefs.current.map((titleRef) => {
+            const { top } = titleRef.getBoundingClientRect()
+
+            return Math.abs(top - tabsBottom)
+        })
+
+        const activeIndex = diffs.indexOf(Math.min(...diffs))
+
+        setActiveTabType(ingredientTypes[activeIndex].type)
+    }
+
+    const tabClickHandler = (type, index) => {
+        setActiveTabType(type)
+        titlesRefs.current[index].scrollIntoView({ behavior: 'smooth' })
+    }
+
     const renderModal = () => (
         <Modal title="Детали ингредиента" onClose={handleModalClose}>
             <IngredientDetails ingredient={activeIngredient} />
         </Modal>
     )
 
-    const renderTypeTab = ({ type, name }) => (
+    const renderTypeTab = ({ type, name }, index) => (
         <Tab
             key={type}
             value={type}
             active={activeTabType === type}
-            onClick={() => setActiveTabType(type)}
+            onClick={() => tabClickHandler(type, index)}
         >
             {name}
         </Tab>
     )
 
-    const renderIngredients = ({ type, name }) => (
-        <div key={`ingredient-${type}`}>
+    const renderIngredients = ({ type, name }, index) => (
+        <div
+            key={`ingredient-${type}`}
+            ref={(el) => {
+                titlesRefs.current[index] = el
+            }}
+        >
             <h2 className="text text_type_main-medium mb-6 mt-10">{name}</h2>
 
             <div className={`${styles.list} pl-4 pr-4`}>
-                {ingredientsDict[type].map((ingredient, index) => (
+                {ingredientsDict[type].map((ingredient) => (
                     <IngredientCard
                         key={ingredient._id}
                         ingredient={ingredient}
-                        count={index % 5}
+                        count={1}
                         onClick={() => showIngredientDetails(ingredient)}
                     />
                 ))}
@@ -96,12 +123,17 @@ export default function BurgerIngredients() {
                 Соберите бургер
             </h1>
 
-            <div className={styles.tabs}>
-                {ingredientTypes.map(renderTypeTab)}
+            <div className={styles.tabs} ref={tabsRef}>
+                {ingredientTypes.map((type, index) =>
+                    renderTypeTab(type, index)
+                )}
             </div>
 
             {ingredients.length > 0 && (
-                <div className={`${styles.ingredients} custom-scroll`}>
+                <div
+                    className={`${styles.ingredients} custom-scroll`}
+                    onScroll={scrollHandler}
+                >
                     {ingredientTypes.map(renderIngredients)}
                 </div>
             )}
