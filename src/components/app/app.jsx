f4ddfcaab1bd95a3bbd73b8192ee1ceaa019aa8e
asharabanova <n.sharabanova@gmail.com>
2024-06-16 14:25:06 +0300

    404 page

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,14 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'
 import AppHeader from '../app-header/app-header'
-import { Home } from '../../pages'
+import { Home, NotFound } from '../../pages'
 
 export default function App() {
     return (
         <div>
             <AppHeader />
             <Routes>
-                <Route path="/" element={<Home />}/>
+                <Route path="/" element={<Home />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </div>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,2 @@
 export { Home } from './home/home'
+export { NotFound } from './not-found/not-found'
